dbc4cc50364c41b847d7092dbc9b1daaa271d7e2
GGuppyGit <kylanplunk@gmail.com>
2023-09-14 08:53:22 -0500

    Used for clearing the database while testing

---
+++ dropdb.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const Players = require('./db.js');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Are you sure you want to drop the database? (y/n)", function(answer) {
+    if (answer == "y") {
+        rl.close();
+        Players.drop();
+    } else {
+        console.log("Aborting");
+        rl.close();
+    }
+});
