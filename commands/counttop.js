39bab8ad56d84f8c2f44c77f0809875ca0d865f8
GGuppyGit <kylanplunk@gmail.com>
2023-11-14 10:56:43 -0600

    pages done

--- commands/counttop.js
+++ commands/counttop.js
@@ -6,16 +6,54 @@ module.exports = {
 		.setName('counttop')
 		.setDescription('Returns a list of the top counters.'),
 	async execute(interaction) {
+
+
 		Counter.sync();
-		const topCounters = await Counter.findAll({ order: [['count', 'DESC']], limit: 10 });
-		const topCounterStrings = topCounters.map(counter => `${counter.countee}: ${counter.count}`);
-		const backButton = new ButtonBuilder().setCustomId('back').setLabel('<').setStyle(ButtonStyle.Secondary);
-		const forwButton = new ButtonBuilder().setCustomId('forward').setLabel('>').setStyle(ButtonStyle.Secondary);
-		const row = new ActionRowBuilder().addComponents(backButton, forwButton);
+		const allCounters = await Counter.findAll({ order: [['count', 'DESC']] });
+
+		for (const counter of allCounters) {
+			// Rank them
+			counter.rank = allCounters.indexOf(counter) + 1;
+		}
+		let currentPageNum = 1;
+
+		function getNewPage() {
+			const currentPageContents = allCounters.slice((currentPageNum - 1) * 10, currentPageNum * 10);
+
+			const topCounterStrings = currentPageContents.map(counter => `${counter.rank}. ${counter.countee}: ${counter.count}`);
+			return `# Top Counts\n${topCounterStrings.join('\n')} \n Page ${currentPageNum} of ${Math.ceil(allCounters.length / 10)}`;
+
+		}
+
+		// Buttons
+		const backardButton = new ButtonBuilder().setCustomId('back').setLabel('<').setStyle(ButtonStyle.Secondary);
+		const forwardButton = new ButtonBuilder().setCustomId('forward').setLabel('>').setStyle(ButtonStyle.Secondary);
+		const row = new ActionRowBuilder().addComponents(backardButton, forwardButton);
 
 		await interaction.reply({
-			content: `# Top Counters\n${topCounterStrings.join('\n')}`,
+			content: getNewPage(),
 			components: [row],
 		});
+
+
+		const filter = i => i.user.id === interaction.user.id;
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 300000 });
+
+		collector.on('collect', m => {
+			if (m.customId === 'back') {
+				if (currentPageNum > 1) {
+					currentPageNum--;
+				}
+				m.update({ content:  getNewPage() });
+			}
+			else if (m.customId === 'forward') {
+				if (currentPageNum < Math.ceil(allCounters.length / 10)) {
+					currentPageNum++;
+				}
+				m.update({ content: getNewPage() });
+			}
+		});
+
+
 	},
 };
